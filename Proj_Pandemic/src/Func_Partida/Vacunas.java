ea740d7ac3b08e47447a6ee4c1785b7cfbe7a82c
ArduiP <162721278+ArduiP@users.noreply.github.com>
2024-04-08 17:53:48 +0200

    Clases por mí
    asdf

---
+++ Proj_Pandemic/src/Func_Partida/Vacunas.java
@@ -0,0 +1,36 @@
+package Func_Partida;
+
+public class Vacunas {
+	private String nombre;
+	private String color;
+	private float porcentaje;
+	
+	public void desarrollarVacuna ( float porcentage) {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public float getPorcentaje() {
+		return porcentaje;
+	}
+
+	public void setPorcentaje(float porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+
+}
